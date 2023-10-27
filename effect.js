037a806abaf8003f37e5c37b125f2053e8ede117
Ashish Kumar Verma <61198989+imdigitalashish@users.noreply.github.com>
2023-10-27 08:33:26 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -6,7 +6,7 @@ class Effect {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.particles = [];
-        this.numberOfParticles = 200;
+        this.numberOfParticles = 30;
         this.createParticles();
     }
 
